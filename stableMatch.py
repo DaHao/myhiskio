7c4fe4ee49d601a5e9f7e702c64ef26c475efb7c
ShengHaoChen <jiash561@gmail.com>
2018-09-27 17:48:22 +0800

    Finish stable matching algorithm.

--- stableMatch.py
+++ stableMatch.py
@@ -84,68 +84,60 @@ class ChineseCupid():
             self.manList.append(Human(key, value))
 
         self.womanList = []
-        for key, value in loverData['woman'].items():
+        for key, value in loveData['woman'].items():
             self.womanList.append(Human(key, value))
-
-# everyone status set to free
-# while ( man is free & not yet woman proposed ) do
-#         woman = highest ranked in man list & man not yet to proposed
-#         if ( woman is free ) then
-#                 ( man, woman ) set engaged
-#         else if ( woman is engaged but man rank high than current man ) then 
-#                 current man set free
-#                 ( man, woman ) set engaged
-# return result
 
     def findHuman(self, name):
         return next(man for man in self.manList + self.womanList if man.name == name)
 
     def findBetterHuman(self, man, name1, name2):
         index1 = man.loverList.index(name1)
-        index2 = man.lvoerList.index(name2)
+        index2 = man.loverList.index(name2)
         return name1 if index1 < index2 else name2
 
     def match(self):
         while not all(man.status == Relationship.married for man in self.manList):
-            man = next(man for man in self.manList 
-                    if man.status == Relationship.free or man.status == Relationship.engaged)
+            man = next(man for man in self.manList if man.status == Relationship.free)
+            print('{0} want to find a lover......'.format(man.name))
 
             for womanName in man.loverList: 
-                woman = findHuman(womanName)
+                woman = self.findHuman(womanName)
+                print('{0} is his target...'.format(woman.name))
 
                 if woman.status == Relationship.free:
                     woman.status = Relationship.engaged
                     woman.lover = man.name 
                     man.status = Relationship.engaged
                     man.lover = woman.name
+                    print('{0} and {1} have got engaged!\n'.format(man.name, woman.name))
                     break
 
-                else if woman.status == Relationship.engaged 
-                        and findBetterHuman(woman, man.name, woman.lover) == man.name:
-                    fiance = findHuman(woman.lover)
+                elif woman.status == Relationship.engaged and self.findBetterHuman(woman, man.name, woman.lover) == man.name:
+                    fiance = self.findHuman(woman.lover)
                     fiance.status = Relationship.free
                     fiance.lover = ""
                     woman.status = Relationship.engaged
                     woman.lover = man.name
                     man.status = Relationship.engaged
                     man.lover = woman.name
+                    print('{0} lost his lover ...QQ'.format(fiance.name))
+                    print('{0} and {1} have got engaged!\n'.format(man.name, woman.name))
                     break
 
             if all(man.status == Relationship.engaged for man in self.manList):
                 for man in self.manList:
                     man.status = Relationship.married
+                print('everyone is engaged !!\n\n')
 
     def showMatch(self):
+        print('The stable Matching is :')
         for man in self.manList:
-            loverName = '' if not man.lover else man.lover.name
-            print(man.name, ' lover is ', loverName)
-
-    
+            print(man.name, ' lover is ', man.lover)
 
 if __name__ == '__main__':
     with open('perference.json', 'r') as f:
         data = json.load(f)
 
     cc = ChineseCupid(data)
-    cc.Match()
+    cc.match()
     cc.showMatch()
