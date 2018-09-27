8ef7024bc55d6398d3780e19a9b8e5c5076db1fa
ShengHaoChen <jiash561@gmail.com>
2018-09-27 17:23:43 +0800

    version 1

--- stableMatch.py
+++ stableMatch.py
@@ -43,12 +43,38 @@ class Relationship(Enum):
 
 class Human():
     def __init__(self, name, lovelist):
-        self.name = name
-        self.status = Relationship.free
-        self.loverList = lovelist
+        self.__name = name
+        self.__status = Relationship.free
+        self.__loverList = lovelist
+        self.__lover = ""
 
-    def setLover(self, human):
-        self.lover = human
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def loverList(self):
+        return self.__loverList
+
+    @property
+    def lover(self):
+        return self.__lover
+
+    @lover.setter
+    def lover(self, name):
+        self.__lover = name
+
+    @property
+    def status(self):
+        return self.__status
+
+    @status.setter
+    def status(self, value):
+        self.__status = value
 
 class ChineseCupid():
 
@@ -57,13 +83,69 @@ class ChineseCupid():
         for key, value in loveData['man'].items():
             self.manList.append(Human(key, value))
 
-    def Match(self):
-        pprint(self.manList)
+        self.womanList = []
+        for key, value in loverData['woman'].items():
+            self.womanList.append(Human(key, value))
 
-if __name__ == "__main__":
+# everyone status set to free
+# while ( man is free & not yet woman proposed ) do
+#         woman = highest ranked in man list & man not yet to proposed
+#         if ( woman is free ) then
+#                 ( man, woman ) set engaged
+#         else if ( woman is engaged but man rank high than current man ) then 
+#                 current man set free
+#                 ( man, woman ) set engaged
+# return result
 
+    def findHuman(self, name):
+        return next(man for man in self.manList + self.womanList if man.name == name)
+
+    def findBetterHuman(self, man, name1, name2):
+        index1 = man.loverList.index(name1)
+        index2 = man.lvoerList.index(name2)
+        return name1 if index1 < index2 else name2
+
+    def match(self):
+        while not all(man.status == Relationship.married for man in self.manList):
+            man = next(man for man in self.manList 
+                    if man.status == Relationship.free or man.status == Relationship.engaged)
+
+            for womanName in man.loverList: 
+                woman = findHuman(womanName)
+
+                if woman.status == Relationship.free:
+                    woman.status = Relationship.engaged
+                    woman.lover = man.name 
+                    man.status = Relationship.engaged
+                    man.lover = woman.name
+                    break
+
+                else if woman.status == Relationship.engaged 
+                        and findBetterHuman(woman, man.name, woman.lover) == man.name:
+                    fiance = findHuman(woman.lover)
+                    fiance.status = Relationship.free
+                    fiance.lover = ""
+                    woman.status = Relationship.engaged
+                    woman.lover = man.name
+                    man.status = Relationship.engaged
+                    man.lover = woman.name
+                    break
+
+            if all(man.status == Relationship.engaged for man in self.manList):
+                for man in self.manList:
+                    man.status = Relationship.married
+
+    def showMatch(self):
+        for man in self.manList:
+            loverName = '' if not man.lover else man.lover.name
+            print(man.name, ' lover is ', loverName)
+
+    
+
+if __name__ == '__main__':
     with open('perference.json', 'r') as f:
         data = json.load(f)
 
     cc = ChineseCupid(data)
     cc.Match()
+    cc.showMatch()
